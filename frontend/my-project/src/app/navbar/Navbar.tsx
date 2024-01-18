ab5f0f064480e3cfaa925d2e44e89ef36ec3a8bc
Mayur Devle <mayurd0303@gmail.com>
2024-01-18 12:23:40 +0000

    remove default btn

--- frontend/my-project/src/app/navbar/Navbar.tsx
+++ frontend/my-project/src/app/navbar/Navbar.tsx
@@ -11,13 +11,9 @@ export default function Navbar() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LFGHO</span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Connect Wallet
-            </button>
+            
             <ConnectKitButton></ConnectKitButton>
+            
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
