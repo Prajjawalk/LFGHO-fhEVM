1f958f46e6e36e8ecc26fbdec36dae0ab11375d9
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-01-19 16:59:36 +0530

    Changes in FHEVMFacilitator.sol

--- contracts/FHEVMFacilitator.sol
+++ contracts/FHEVMFacilitator.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import { IGhoToken } from "./interfaces/IGhoToken.sol";
 import { IPrivateGho } from "./interfaces/IPrivateGho.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import "fhevm/abstracts/EIP712WithModifier.sol";
 import "fhevm/lib/TFHE.sol";
 
@@ -11,23 +12,23 @@ import "fhevm/lib/TFHE.sol";
 // total mintable cap will be set to the amount of gho received from the bridge
 
 contract FHEVMFacilitator is EIP712WithModifier {
-    IGhoToken public ghoToken;
     IPrivateGho public privateGho;
     IERC20 public usdcToken; // mock USDC
     IERC20 public hypGhoToken; // wrapped GHO
-    euint32 internal totalMintableCap; // matches the amount of gho bridged
+    // euint32 internal totalMintableCap; // matches the amount of gho bridged
+    euint32 internal currentMinted;
 
     mapping(address user => euint32 collateral) public collateralSupplied;
     mapping(address user => euint32 borrowed) public ghoBorrowed;
 
     constructor(
-        IGhoToken _ghoToken,
         IPrivateGho _privateGho,
-        IERC20 _usdcToken
+        IERC20 _usdcToken,
+        IERC20 _hypGHOToken,
     ) EIP712WithModifier("FHEVMFacilitator", "1") {
-        ghoToken = _ghoToken;
         privateGho = _privateGho;
         usdcToken = _usdcToken;
+        hypGHOToken = _hypGHOToken;
     }
 
     // TODO: function to receive hypGHO
@@ -53,10 +54,10 @@ contract FHEVMFacilitator is EIP712WithModifier {
     function mintEncryptedGho(address account, bytes calldata encryptedAmount) external {
         euint32 amount = TFHE.asEuint32(encryptedAmount);
         ebool hasEnoughCollateral = TFHE.le(TFHE.add(ghoBorrowed[account], amount), collateralSupplied[account]);
-        ebool withinMintableCap = TFHE.le(amount, totalMintableCap);
+        currentMinted = TFHE.add(currentMinted, amount);
+        ebool withinMintableCap = TFHE.le(currentMinted, TFHE.asEuint32(hypGHOToken.totalSupply()));
         privateGho.mint(account, amount);
         ghoBorrowed[account] = TFHE.add(ghoBorrowed[account], amount);
-        totalMintableCap = TFHE.sub(totalMintableCap, amount);
         TFHE.optReq(hasEnoughCollateral);
         TFHE.optReq(withinMintableCap);
     }
@@ -67,7 +68,7 @@ contract FHEVMFacilitator is EIP712WithModifier {
         TFHE.optReq(sufficientBalance);
         privateGho.burn(account, amount);
         ghoBorrowed[account] = TFHE.sub(ghoBorrowed[account], amount);
-        totalMintableCap = TFHE.add(totalMintableCap, amount);
+        currentMinted = TFHE.sub(currentMinted, amount);
     }
 
     function getMaxMintableAmount(
@@ -78,7 +79,8 @@ contract FHEVMFacilitator is EIP712WithModifier {
         euint32 currentBorrowed = ghoBorrowed[account];
         euint32 currentCollateral = collateralSupplied[account];
         euint32 remainingCollateralCapacity = TFHE.sub(currentCollateral, currentBorrowed);
-        euint32 maxMintable = TFHE.min(remainingCollateralCapacity, totalMintableCap);
+
+        euint32 maxMintable = TFHE.min(remainingCollateralCapacity, TFHE.sub(TFHE.asEuint32(hypGHOToken.totalSupply()), currentMinted));
         return TFHE.reencrypt(maxMintable, publicKey);
     }
 }
