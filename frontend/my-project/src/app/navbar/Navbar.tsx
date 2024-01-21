9cee5db63c049f8a0ff8bd460b9d49644236ecfa
Mayur Devle <mayurd0303@gmail.com>
2024-01-21 10:24:21 +0000

    update logo

--- frontend/my-project/src/app/navbar/Navbar.tsx
+++ frontend/my-project/src/app/navbar/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="" className="h-8" alt="" />
+          <Link href="/supplies" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="/logo.svg" className="h-8" alt="logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Crypto <span className="text-[#17B9BB]">Cafe</span> </span>
           </Link>
 
